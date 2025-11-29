608b0b8a65fa960567471661328c0ad55b560557
futurepraxis <61433938+futurepraxis@users.noreply.github.com>
2025-11-29 05:38:41 -0600

    Create variables to store form elements

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+//Store inputs
+const fortuneForm = document.getElementById('fortuneForm');
+const moodDropdown = document.getElementById('moodDropdown');
+const typeDropdown = document.getElementById('typeDropdown');
+const topicDropdown = document.getElementById('topicDropdown');
+const generateBtn = document.getElementById('generateBtn');
+const surpriseBtn = document.getElementById('surpriseBtn');
