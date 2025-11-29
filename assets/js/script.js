16097534b6564b5ad33fdc01e076b0a5b8f3b580
futurepraxis <61433938+futurepraxis@users.noreply.github.com>
2025-11-29 07:54:42 -0600

    Created basic function to call cat API and display image. Created function to update results with cat image. Created function to display error message.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,45 @@
+//API URLS
+const CAT_URL = 'https://cataas.com';
+
 //Store inputs
 const fortuneForm = document.getElementById('fortuneForm');
-const moodDropdown = document.getElementById('moodDropdown');
+// const moodDropdown = document.getElementById('moodDropdown');
 const typeDropdown = document.getElementById('typeDropdown');
 const topicDropdown = document.getElementById('topicDropdown');
 const generateBtn = document.getElementById('generateBtn');
 const surpriseBtn = document.getElementById('surpriseBtn');
+const catImg = document.getElementById('catImg');
+const errorMessage = document.getElementById('errorMessage');
+
+//Get values from form
+fortuneForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // mood = moodDropdown.options[moodDropdown.selectedIndex].value;
+    type = typeDropdown.options[typeDropdown.selectedIndex].value;
+    topic = topicDropdown.options[topicDropdown.selectedIndex].value;
+    catQuery(type);
+})
+
+async function catQuery(type) {
+    const url = `${CAT_URL}/cat/${type}`;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        updateResults(response.url);
+    } catch (error) {
+        console.error(error.message);
+        displayErrorMessage();
+    }
+};
+
+function updateResults(url) {
+    errorMessage.classList.toggle('is-hidden');
+    document.getElementById('result').classList.remove('is-hidden');
+    catImg.src = url;
+};
+
+function displayErrorMessage() {
+     errorMessage.classList.toggle('is-hidden');
+}
