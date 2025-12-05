f7186dce057fb960614cb375e2ae9510f6089d3e
Geo <gvong@protonmail.com>
2025-12-04 20:14:46 -0500

    Separated the navbar js into another file because it was being mean to me

---
+++ assets/js/navbar.js
@@ -0,0 +1,17 @@
+// I separated the navbar function because it wasn't playing nicely while embedded within the other 2 js files. -GV
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  // Add a click event on each of them
+  $navbarBurgers.forEach( el => {
+    el.addEventListener('click', () => {
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
+    });
+  });
+});
