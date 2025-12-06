62f57322a794f7374ae6cb68f6b94ff25bf7b75b
futurepraxis <61433938+futurepraxis@users.noreply.github.com>
2025-12-05 18:49:25 -0600

    Fixed spacing on favorites page empty state (no favorites yet) view

--- assets/js/favorites.js
+++ assets/js/favorites.js
@@ -36,7 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
 
     } else {
-        favoritesSection.innerHTML = `<p class="has-text-centered" style="min-height:82vh;">Sorry, you don't have any favorites yet.</p>`
+        favoritesSection.innerHTML = `<p class="has-text-centered" style="min-height:70vh;">Sorry, you don't have any favorites yet.</p>`
     }
 });
 
